129f3f0bdd39a51850ef86b5e0d553f6157d4361
Melissa <ha@melissadata.com>
2022-12-29 09:56:34 -0800

    Commit Python3 sample code for Email Object

---
+++ MelissaDataEmailObjectWindowsPython3Sample/MelissaDataEmailObjectWindowsPython3Sample.py
@@ -0,0 +1,156 @@
+import mdEmail_pythoncode
+import os
+import sys
+import json
+
+
+class DataContainer:
+    def __init__(self, email="", result_codes=[]):
+        self.email = email
+        self.result_codes = result_codes
+
+class EmailObject:
+    """ Set license string and set path to data files  (.dat, etc) """
+    def __init__(self, license, data_path):
+        self.md_email_obj = mdEmail_pythoncode.mdEmail()
+        self.md_email_obj.SetLicenseString(license)
+        self.data_path = data_path
+
+        """
+        If you see a different date than expected, check your license string and either download the new data files or use the Melissa Updater program to update your data files.  
+        """
+
+        self.md_email_obj.SetPathToEmailFiles(data_path)
+        p_status = self.md_email_obj.InitializeDataFiles()
+        
+
+        if (p_status != mdEmail_pythoncode.ProgramStatus.ErrorNone):
+            print("Failed to Initialize Object.")
+            print(p_status)
+            return
+        
+        print(f"                DataBase Date: {self.md_email_obj.GetDatabaseDate()}")
+        print(f"              Expiration Date: {self.md_email_obj.GetLicenseStringExpirationDate()}")
+      
+        """
+        This number should match with file properties of the Melissa Data Object binary file.
+        If TEST appears with the build number, there may be a license key issue.
+        """
+        print(f"               Object Version: {self.md_email_obj.GetBuildNumber()}\n")
+    
+
+    def execute_object_and_result_codes(self, data):
+        self.md_email_obj.SetCacheUse(1)
+        self.md_email_obj.SetCorrectSyntax(True)
+        self.md_email_obj.SetDatabaseLookup(True)
+        self.md_email_obj.SetFuzzyLookup(True)
+        self.md_email_obj.SetMXLookup(True)
+        self.md_email_obj.SetStandardizeCasing(True)
+        self.md_email_obj.SetWSLookup(False)
+
+        self.md_email_obj.VerifyEmail(data.email)
+        result_codes = self.md_email_obj.GetResults()
+
+        """ 
+        ResultsCodes explain any issues email object has with the object.
+        List of result codes for Email object
+        https://wiki.melissadata.com/?title=Result_Code_Details#Email_Object
+        """
+
+        return DataContainer(data.email, result_codes)
+
+
+def parse_arguments():
+    license, test_email, data_path = "", "", ""
+
+    args = sys.argv
+    index = 0
+    for arg in args:
+        
+        if (arg == "--license") or (arg == "-l"):
+            if (args[index+1] != None):
+                license = args[index+1]
+        if (arg == "--email") or (arg == "-p"):
+            if (args[index+1] != None):
+                test_email = args[index+1]
+        if (arg == "--dataPath") or (arg == "-d"):
+            if (args[index+1] != None):
+                data_path = args[index+1]
+        index += 1
+
+    return (license, test_email, data_path)
+
+def run_as_console(license, test_email, data_path):
+    print("\n\n===== WELCOME TO MELISSA DATA EMAIL OBJECT WINDOWS PYTHON3 SAMPLE =====\n")
+
+    email_object = EmailObject(license, data_path)
+
+    should_continue_running = True
+
+    if email_object.md_email_obj.GetInitializeErrorString() != "No error.":
+      should_continue_running = False
+      
+    while should_continue_running:
+        if test_email == None or test_email == "":        
+          print("\nFill in each value to see the Email Object results")
+          email = str(input("Email: "))
+        else:        
+          email = test_email
+        
+        data = DataContainer(email)
+
+        """ Print user input """
+        print("\n================================ INPUTS ===============================\n")
+        print(f"\t               Email: {email}")
+
+        """ Execute Email Object """
+        data_container = email_object.execute_object_and_result_codes(data)
+
+        """ Print output """
+        print("\n================================ OUTPUT ===============================\n")
+        print("\n\tEmail Object Information:")
+
+        print(f"\t                    Email: {data_container.email}")
+        print(f"\t              MailBoxName: {email_object.md_email_obj.GetMailBoxName()}")
+        print(f"\t               DomainName: {email_object.md_email_obj.GetDomainName()}")
+        print(f"\t           TopLevelDomain: {email_object.md_email_obj.GetTopLevelDomain()}")
+        print(f"\tTopLevelDomainDescription: {email_object.md_email_obj.GetTopLevelDomainDescription()}")
+        print(f"\t             Result Codes: {data_container.result_codes}")
+
+
+
+        rs = data_container.result_codes.split(',')
+        for r in rs:
+            print(f"        {r}: {email_object.md_email_obj.GetResultCodeDescription(r, mdEmail_pythoncode.ResultCdDescOpt.ResultCodeDescriptionLong)}")
+
+
+        is_valid = False
+        if not (test_email == None or test_email == ""):
+            is_valid = True
+            should_continue_running = False    
+        while not is_valid:
+        
+            test_another_response = input(str("\nTest another email? (Y/N)\n"))
+            
+
+            if not (test_another_response == None or test_another_response == ""):         
+                test_another_response = test_another_response.lower()
+            if test_another_response == "y":
+                is_valid = True
+            
+            elif test_another_response == "n":
+                is_valid = True
+                should_continue_running = False            
+            else:
+            
+              print("Invalid Response, please respond 'Y' or 'N'")
+
+    print("\n============ THANK YOU FOR USING MELISSA DATA PYTHON3 OBJECT ==========\n")
+    
+
+
+"""  MAIN STARTS HERE   """
+
+license, test_email, data_path = parse_arguments()
+
+run_as_console(license, test_email, data_path)
---
+++ MelissaDataEmailObjectWindowsPython3Sample/mdEmail_pythoncode.py
@@ -0,0 +1,232 @@
+from ctypes import *
+from enum import Enum
+import ctypes
+import os
+
+dir = os.getcwd()
+
+
+if (os.name == 'nt'):
+  lib = ctypes.CDLL(f'{dir}/MelissaDataEmailObjectWindowsPython3Sample/mdEmail.dll')
+else:
+  lib = ctypes.CDLL('libmdPhone.so')
+
+
+lib.mdEmailCreate.argtypes = []
+lib.mdEmailCreate.restype = c_void_p
+lib.mdEmailDestroy.argtypes = [c_void_p]
+lib.mdEmailDestroy.restype = None
+lib.mdEmailSetLicenseString.argtypes = [c_void_p, c_char_p]
+lib.mdEmailSetLicenseString.restype = c_bool
+lib.mdEmailSetPathToEmailFiles.argtypes = [c_void_p, c_char_p]
+lib.mdEmailSetPathToEmailFiles.restype = None
+lib.mdEmailInitializeDataFiles.argtypes = [c_void_p]
+lib.mdEmailInitializeDataFiles.restype = c_int
+lib.mdEmailGetInitializeErrorString.argtypes = [c_void_p]
+lib.mdEmailGetInitializeErrorString.restype = c_char_p
+lib.mdEmailGetBuildNumber.argtypes = [c_void_p]
+lib.mdEmailGetBuildNumber.restype = c_char_p
+lib.mdEmailGetDatabaseDate.argtypes = [c_void_p]
+lib.mdEmailGetDatabaseDate.restype = c_char_p
+lib.mdEmailGetDatabaseExpirationDate.argtypes = [c_void_p]
+lib.mdEmailGetDatabaseExpirationDate.restype = c_char_p
+lib.mdEmailGetLicenseStringExpirationDate.argtypes = [c_void_p]
+lib.mdEmailGetLicenseStringExpirationDate.restype = c_char_p
+lib.mdEmailVerifyEmail.argtypes = [c_void_p, c_char_p]
+lib.mdEmailVerifyEmail.restype = c_bool
+lib.mdEmailSetCorrectSyntax.argtypes = [c_void_p, c_bool]
+lib.mdEmailSetCorrectSyntax.restype = None
+lib.mdEmailSetUpdateDomain.argtypes = [c_void_p, c_bool]
+lib.mdEmailSetUpdateDomain.restype = None
+lib.mdEmailSetDatabaseLookup.argtypes = [c_void_p, c_bool]
+lib.mdEmailSetDatabaseLookup.restype = None
+lib.mdEmailSetFuzzyLookup.argtypes = [c_void_p, c_bool]
+lib.mdEmailSetFuzzyLookup.restype = None
+lib.mdEmailSetWSLookup.argtypes = [c_void_p, c_bool]
+lib.mdEmailSetWSLookup.restype = None
+lib.mdEmailSetWSMailboxLookup.argtypes = [c_void_p, c_int]
+lib.mdEmailSetWSMailboxLookup.restype = None
+lib.mdEmailSetMXLookup.argtypes = [c_void_p, c_bool]
+lib.mdEmailSetMXLookup.restype = None
+lib.mdEmailSetStandardizeCasing.argtypes = [c_void_p, c_bool]
+lib.mdEmailSetStandardizeCasing.restype = None
+lib.mdEmailGetStatusCode.argtypes = [c_void_p]
+lib.mdEmailGetStatusCode.restype = c_char_p
+lib.mdEmailGetErrorCode.argtypes = [c_void_p]
+lib.mdEmailGetErrorCode.restype = c_char_p
+lib.mdEmailGetErrorString.argtypes = [c_void_p]
+lib.mdEmailGetErrorString.restype = c_char_p
+lib.mdEmailGetChangeCode.argtypes = [c_void_p]
+lib.mdEmailGetChangeCode.restype = c_uint
+lib.mdEmailGetResults.argtypes = [c_void_p]
+lib.mdEmailGetResults.restype = c_char_p
+lib.mdEmailGetResultCodeDescription.argtypes = [c_void_p, c_char_p, c_int]
+lib.mdEmailGetResultCodeDescription.restype = c_char_p
+lib.mdEmailGetMailBoxName.argtypes = [c_void_p]
+lib.mdEmailGetMailBoxName.restype = c_char_p
+lib.mdEmailGetDomainName.argtypes = [c_void_p]
+lib.mdEmailGetDomainName.restype = c_char_p
+lib.mdEmailGetTopLevelDomain.argtypes = [c_void_p]
+lib.mdEmailGetTopLevelDomain.restype = c_char_p
+lib.mdEmailGetTopLevelDomainDescription.argtypes = [c_void_p]
+lib.mdEmailGetTopLevelDomainDescription.restype = c_char_p
+lib.mdEmailGetEmailAddress.argtypes = [c_void_p]
+lib.mdEmailGetEmailAddress.restype = c_char_p
+lib.mdEmailSetReserved.argtypes = [c_void_p, c_char_p, c_char_p]
+lib.mdEmailSetReserved.restype = None
+lib.mdEmailGetReserved.argtypes = [c_void_p, c_char_p]
+lib.mdEmailGetReserved.restype = c_char_p
+lib.mdEmailSetCachePath.argtypes = [c_void_p, c_char_p]
+lib.mdEmailSetCachePath.restype = None
+lib.mdEmailSetCacheUse.argtypes = [c_void_p, c_int]
+lib.mdEmailSetCacheUse.restype = None
+
+# mdEmail Enumerations
+class ProgramStatus(Enum):
+	ErrorNone = 0
+	ErrorOther = 1
+	ErrorOutOfMemory = 2
+	ErrorRequiredFileNotFound = 3
+	ErrorFoundOldFile = 4
+	ErrorDatabaseExpired = 5
+	ErrorLicenseExpired = 6
+
+class AccessType(Enum):
+	Local = 0
+	Remote = 1
+
+class DiacriticsMode(Enum):
+	Auto = 0
+	On = 1
+	Off = 2
+
+class StandardizeMode(Enum):
+	ShortFormat = 0
+	LongFormat = 1
+	AutoFormat = 2
+
+class SuiteParseMode(Enum):
+	ParseSuite = 0
+	CombineSuite = 1
+
+class AliasPreserveMode(Enum):
+	ConvertAlias = 0
+	PreserveAlias = 1
+
+class AutoCompletionMode(Enum):
+	AutoCompleteSingleSuite = 0
+	AutoCompleteRangedSuite = 1
+	AutoCompletePlaceHolderSuite = 2
+	AutoCompleteNoSuite = 3
+
+class ResultCdDescOpt(Enum):
+	ResultCodeDescriptionLong = 0
+	ResultCodeDescriptionShort = 1
+
+class MailboxLookupMode(Enum):
+	MailboxNone = 0
+	MailboxExpress = 1
+	MailboxPremium = 2
+
+class mdEmail(object):
+	def __init__(self):
+		self.I = lib.mdEmailCreate()
+
+	def __del__(self):
+		lib.mdEmailDestroy(self.I)
+
+	def SetLicenseString(self, License):
+		return lib.mdEmailSetLicenseString(self.I, License.encode('utf-8'))
+
+	def SetPathToEmailFiles(self, emailDataFiles):
+		lib.mdEmailSetPathToEmailFiles(self.I, emailDataFiles.encode('utf-8'))
+
+	def InitializeDataFiles(self):
+		return ProgramStatus(lib.mdEmailInitializeDataFiles(self.I))
+
+	def GetInitializeErrorString(self):
+		return lib.mdEmailGetInitializeErrorString(self.I).decode('utf-8')
+
+	def GetBuildNumber(self):
+		return lib.mdEmailGetBuildNumber(self.I).decode('utf-8')
+
+	def GetDatabaseDate(self):
+		return lib.mdEmailGetDatabaseDate(self.I).decode('utf-8')
+
+	def GetDatabaseExpirationDate(self):
+		return lib.mdEmailGetDatabaseExpirationDate(self.I).decode('utf-8')
+
+	def GetLicenseStringExpirationDate(self):
+		return lib.mdEmailGetLicenseStringExpirationDate(self.I).decode('utf-8')
+
+	def VerifyEmail(self, email):
+		return lib.mdEmailVerifyEmail(self.I, email.encode('utf-8'))
+
+	def SetCorrectSyntax(self, p1):
+		lib.mdEmailSetCorrectSyntax(self.I, p1)
+
+	def SetUpdateDomain(self, p1):
+		lib.mdEmailSetUpdateDomain(self.I, p1)
+
+	def SetDatabaseLookup(self, p1):
+		lib.mdEmailSetDatabaseLookup(self.I, p1)
+
+	def SetFuzzyLookup(self, p1):
+		lib.mdEmailSetFuzzyLookup(self.I, p1)
+
+	def SetWSLookup(self, p1):
+		lib.mdEmailSetWSLookup(self.I, p1)
+
+	def SetWSMailboxLookup(self, mailboxLookupmode):
+		lib.mdEmailSetWSMailboxLookup(self.I, MailboxLookupMode(mailboxLookupmode).value)
+
+	def SetMXLookup(self, p1):
+		lib.mdEmailSetMXLookup(self.I, p1)
+
+	def SetStandardizeCasing(self, p1):
+		lib.mdEmailSetStandardizeCasing(self.I, p1)
+
+	def GetStatusCode(self):
+		return lib.mdEmailGetStatusCode(self.I).decode('utf-8')
+
+	def GetErrorCode(self):
+		return lib.mdEmailGetErrorCode(self.I).decode('utf-8')
+
+	def GetErrorString(self):
+		return lib.mdEmailGetErrorString(self.I).decode('utf-8')
+
+	def GetChangeCode(self):
+		return lib.mdEmailGetChangeCode(self.I)
+
+	def GetResults(self):
+		return lib.mdEmailGetResults(self.I).decode('utf-8')
+
+	def GetResultCodeDescription(self, resultCode, opt):
+		return lib.mdEmailGetResultCodeDescription(self.I, resultCode.encode('utf-8'), ResultCdDescOpt(opt).value).decode('utf-8')
+
+	def GetMailBoxName(self):
+		return lib.mdEmailGetMailBoxName(self.I).decode('utf-8')
+
+	def GetDomainName(self):
+		return lib.mdEmailGetDomainName(self.I).decode('utf-8')
+
+	def GetTopLevelDomain(self):
+		return lib.mdEmailGetTopLevelDomain(self.I).decode('utf-8')
+
+	def GetTopLevelDomainDescription(self):
+		return lib.mdEmailGetTopLevelDomainDescription(self.I).decode('utf-8')
+
+	def GetEmailAddress(self):
+		return lib.mdEmailGetEmailAddress(self.I).decode('utf-8')
+
+	def SetReserved(self, Property, Value_):
+		lib.mdEmailSetReserved(self.I, Property.encode('utf-8'), Value_.encode('utf-8'))
+
+	def GetReserved(self, Property_):
+		return lib.mdEmailGetReserved(self.I, Property_.encode('utf-8')).decode('utf-8')
+
+	def SetCachePath(self, cachePath):
+		lib.mdEmailSetCachePath(self.I, cachePath.encode('utf-8'))
+
+	def SetCacheUse(self, cacheUse):
+		lib.mdEmailSetCacheUse(self.I, cacheUse)
