ae87c248d9381e06676ea0e679aafea5ad4fdc1d
Melissa <>
2023-10-20 13:23:23 -0700

    Tested successfully using Melissa data files for 2023.10

--- MelissaEmailObjectWindowsPython3.ps1
+++ MelissaEmailObjectWindowsPython3.ps1
@@ -18,7 +18,7 @@ class FileConfig {
 
 ######################### Config ###########################
 
-$RELEASE_VERSION = '2023.09'
+$RELEASE_VERSION = '2023.10'
 $ProductName = "DQ_EMAIL_DATA"
 
 # Uses the location of the .ps1 file 
